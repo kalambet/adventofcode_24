2527180c95980dc7730d12f3cba189e8c88f1653
Peter Kalambet <peter@kalambet.dev>
2024-12-03 21:06:47 +0100

    day3 input

---
+++ src/day3.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const HashMap = std.AutoHashMap;
+const Tuple = std.meta.Tuple;
+
+
+pub fn count_sums() !void {
+    var file = try std.fs.cwd().openFile("input_day2.txt", .{});
+    defer file.close();
+
+    const file_size = try file.metadata().size();
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    var buf: [file_size]u8 = undefined;
+    in_stream.readAll(&buf);
+    
+}
